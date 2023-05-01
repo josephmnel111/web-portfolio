aff99237d28dd72475219da6803aef0714acaf6c
Joey <josephnm@umich.edu>
2023-04-30 23:14:20 -0400

    build deleted

--- src/index.js
+++ src/index.js
@@ -17,6 +17,10 @@ const router = createBrowserRouter([
     element: <App/>,
   },
   {
+    path: "/web-portfolio",
+    element: <App/>
+  },
+  {
     path: "fitness-app-details",
     element: <FitnessAppDetails/>,
   },
