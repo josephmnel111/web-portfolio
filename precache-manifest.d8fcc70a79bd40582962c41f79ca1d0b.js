f7856bffb7cbbf4563569fefb7867a1859ffbb1e
Joey <josephnm@umich.edu>
2023-04-30 21:51:32 -0400

    Updates

---
+++ precache-manifest.d8fcc70a79bd40582962c41f79ca1d0b.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = [
+  {
+    "revision": "39d17d3cd569d9eba92a",
+    "url": "/web-portfolio/static/css/main.decdfaa8.chunk.css"
+  },
+  {
+    "revision": "39d17d3cd569d9eba92a",
+    "url": "/web-portfolio/static/js/main.91c1e47c.chunk.js"
+  },
+  {
+    "revision": "a99f04d1de7a0a5d3664",
+    "url": "/web-portfolio/static/js/runtime~main.e717f727.js"
+  },
+  {
+    "revision": "ec6dace6128e40d3c3e4",
+    "url": "/web-portfolio/static/js/2.39a192c1.chunk.js"
+  },
+  {
+    "revision": "9417166b9fd15ad1ab18",
+    "url": "/web-portfolio/static/js/3.69c2b8f0.chunk.js"
+  },
+  {
+    "revision": "07bb329b918ea7c9db6f897b5dc63c25",
+    "url": "/web-portfolio/static/media/Resume.07bb329b.pdf"
+  },
+  {
+    "revision": "f864c700213f177b2e9f3fd8f20c9ebe",
+    "url": "/web-portfolio/static/media/laptop.f864c700.png"
+  },
+  {
+    "revision": "73b6f03e5dd75ee8b71d72a3fbb12d0d",
+    "url": "/web-portfolio/static/media/jeopardy_game_display.73b6f03e.PNG"
+  },
+  {
+    "revision": "d066bbda81eb4f4e5e9624ca49d8b682",
+    "url": "/web-portfolio/static/media/fitness_app_display.d066bbda.PNG"
+  },
+  {
+    "revision": "634910d8aee152f8a9332251e55e0d7c",
+    "url": "/web-portfolio/static/media/oplogic_project_display.634910d8.PNG"
+  },
+  {
+    "revision": "1e58835999ee2b44d3e8222ac3c042ed",
+    "url": "/web-portfolio/static/media/Fitness-App-Profile.1e588359.mp4"
+  },
+  {
+    "revision": "f713d6899f55b82aa5eea2b079fb54b7",
+    "url": "/web-portfolio/static/media/Fitness-App-Lifting.f713d689.mp4"
+  },
+  {
+    "revision": "5dacc6eeb7ff4bd5b95d88a3f24c3a7c",
+    "url": "/web-portfolio/static/media/Api-Video.5dacc6ee.mp4"
+  },
+  {
+    "revision": "b8e8848cc0d6be20bd6666a1f440aad0",
+    "url": "/web-portfolio/static/media/Jeopardy_Question_Clip.b8e8848c.mp4"
+  },
+  {
+    "revision": "6883cff367307a9d3921c5c007938ae2",
+    "url": "/web-portfolio/static/media/Oplogic-Create.6883cff3.mp4"
+  },
+  {
+    "revision": "9b7fac9e936c15a6548adbe86a28b846",
+    "url": "/web-portfolio/static/media/OplogicMessagingVideo1.9b7fac9e.mp4"
+  },
+  {
+    "revision": "bd723f16f9cfb076992cda50e106d2af",
+    "url": "/web-portfolio/static/media/OplogicMessagingVideo2.bd723f16.mp4"
+  },
+  {
+    "revision": "ca6fb1f21972ee9aca258cde68179aac",
+    "url": "/web-portfolio/static/media/OplogicTaggingVideo1.ca6fb1f2.mp4"
+  },
+  {
+    "revision": "0a0cd81958972339b8fb9a02757c3eba",
+    "url": "/web-portfolio/static/media/OplogicTaggingVideo2.0a0cd819.mp4"
+  },
+  {
+    "revision": "95b8e177e6fba9f2a61c042404c6abfa",
+    "url": "/web-portfolio/index.html"
+  }
+];
